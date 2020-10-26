198903da7451a53e8e16b955cc8a7eb44768dfd1
Max <maxskavronsky@gmail.com>
2020-10-26 15:41:14 +0300

    добавлена обработка исключений во втором задании, а также выполнено третье

--- homework_2/task_2.py
+++ homework_2/task_2.py
@@ -2,6 +2,11 @@ def evenOrNot(a, even=0, odd=0):
     if a == 0:
         print(f'Количество четных и нечетных цифр в числе равно: ({even}, {odd})')
         return
+    try:
+        a = int(a)
+    except ValueError:
+        print('Ошибка ввода. Попробуйте еще раз.')
+        return evenOrNot(input('Введите число: '))
     num = a % 10
     if num % 2 == 0:
         even+=1
@@ -10,5 +15,4 @@ def evenOrNot(a, even=0, odd=0):
     a = a // 10
     evenOrNot(a, even, odd)
 
-a = int(input('Введите число: '))
-evenOrNot(a)
+evenOrNot(input('Введите число: '))
