bb11598df2904f747b49d8bad9974b838c69e7fc
Max <maxskavronsky@gmail.com>
2020-10-29 19:46:28 +0300

    а если append'ом делать, то при заполнении list быстрее будет

--- homework_3/task_1.py
+++ homework_3/task_1.py
@@ -12,3 +12,30 @@
 то реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к двум своим функциям.
 """
+import time
+
+def calculate_time(func):
+    def inner(*args, **kwargs):
+        begin = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print(f'Время выполнения функции {func.__name__}: {end - begin} секунд')
+
+    return inner
+
+@calculate_time
+def fill_list():
+    list = []
+    for i in range(100000):
+        list.insert(0, i)
+    return list
+
+@calculate_time
+def fill_dict():
+    dikt = {}
+    for i in range(100000):
+        dikt[i]=f'{i}'
+    return dikt
+
+fill_dict()
+fill_list()
