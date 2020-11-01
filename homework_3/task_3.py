d91de551fc1e097a873903ab98ad58ee0e4eb613
Max <maxskavronsky@gmail.com>
2020-11-01 17:33:49 +0300

    не совсем понял смысла исопльзования хеш функции, если только не подразумевался сама функция set()

--- homework_3/task_3.py
+++ homework_3/task_3.py
@@ -1,16 +1,16 @@
-"""
-Задание 3.
-Определить количество различных подстрок с использованием хеш-функции.
-Дана строка S длиной N, состоящая только из строчных латинских букв.
+from hashlib import sha256
 
-Подсказка: примените хеши и множества
+def findSubstrings():
+    result = set()
+    string = input('Введите строку:')
+    n = len(string)
+    for i in range(n+1):
+        for j in range(n+1):
+            var = ''.join(string[i:j])
+            if var:
+                print(var)
+                result.add(sha256(var.encode('utf-8')).hexdigest())
+    print(f'Было найдено {len(result)} различных подстрок в строке "{string}"')
 
-рара:
 
-рар
-ра
-ар
-ара
-р
-а
-"""
+findSubstrings()
