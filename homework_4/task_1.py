2b4978a0b6bc3689903ae645ba0f987d8561e3fb
Max <maxskavronsky@gmail.com>
2020-11-04 23:21:32 +0300

    tuples rulezzzzz

--- homework_4/task_1.py
+++ homework_4/task_1.py
@@ -1,17 +1,4 @@
-"""
-Задание 1.
-
-Приведен код, который позволяет сохранить в
-массиве индексы четных элементов другого массива
-
-Сделайте замеры времени выполнения кода с помощью модуля timeit
-
-Попробуйте оптимизировать код, чтобы снизить время выполнения
-Проведите повторные замеры
-
-Добавьте аналитику: что вы сделали и почему
-"""
-
+import timeit
 
 def func_1(nums):
     new_arr = []
@@ -20,3 +7,11 @@ def func_1(nums):
             new_arr.append(i)
     return new_arr
 
+def func_2(nums):
+    new_nums = [i for i, j in enumerate(nums) if j % 2 == 0] # Enumerate дарит кортеж, который быстрее списка
+    return new_nums
+
+f1_timer = timeit.Timer('func_1([i for i in range(100)])', 'from __main__ import func_1')
+f2_timer = timeit.Timer('func_2([i for i in range(100)])', 'from __main__ import func_2')
+print(f1_timer.timeit(number=10000))
+print(f2_timer.timeit(number=10000))
