ea0afc4b1e4862f5a7975296b5e8119bc8cc039c
Max <maxskavronsky@gmail.com>
2020-11-06 04:55:06 +0300

    weird

--- homework_4/task_3.py
+++ homework_4/task_3.py
@@ -9,11 +9,12 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
-
+from timeit import timeit
+import cProfile
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
-        return
+        return # Возвращает None даже если здесь указать return revers_num, яннп
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
@@ -29,8 +30,18 @@ def revers_2(enter_num, revers_num=0):
     return revers_num
 
 
-def revers_3(enter_num):
+def revers_3(enter_num): # Выигрышное решение, так как нет числовых и логических операций, только строковые
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+input = int(input('Введите число: '))
+
+print(f'Рекурсивное решение: ', timeit(f'revers({input})', setup='from __main__ import revers', number=10000))
+print(f'Цикличное решение: ', timeit(f'revers_2({input})', setup='from __main__ import revers_2', number=10000))
+print(f'Строковое решение: ', timeit(f'revers_3({input})', setup='from __main__ import revers_3', number=10000))
+
+cProfile.run('revers(1000000)')
+cProfile.run('revers_2(1000000)')
+cProfile.run('revers_3(1000000)')
 
