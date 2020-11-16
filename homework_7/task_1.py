1c20ffbb5686bff314e49d2d0e8286444ea173b7
Max <maxskavronsky@gmail.com>
2020-11-16 03:31:30 +0300

    hey bubblesort long time no see bro

--- homework_7/task_1.py
+++ homework_7/task_1.py
@@ -1,14 +1,39 @@
-"""
-1. Отсортируйте по убыванию методом "пузырька" одномерный целочисленный массив,
-заданный случайными числами на промежутке [-100; 100). Выведите на экран
-исходный и отсортированный массивы. Сортировка должна быть реализована в
-виде функции. Обязательно доработайте алгоритм (сделайте его умнее).
+from random import randint
+from timeit import timeit
 
-Идея доработки: если за проход по списку не совершается ни одной сортировки,
-то завершение
-Обязательно сделайте замеры времени обеих реализаций
-и обосновать дала ли оптимизация эффективность
+array_to_sort = [randint(-100, 100) for i in range(100)]
 
-Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
-а по убыванию
-"""
+
+def bubblesort(arr):
+    a = arr
+    n = 1
+    while n < len(a):
+        for el in range(len(a)-n):
+            if a[el] < a[el+1]:
+                a[el], a[el+1] = a[el+1], a[el]
+        n += 1
+    return a
+
+
+def bubblesort_optimized(arr):
+    a = arr
+    n = 1
+    while n < len(a):
+        for el in range(len(a)-n):
+            if a[el] < a[el+1]:
+                a[el], a[el+1] = a[el+1], a[el]
+            else:
+                break
+        n += 1
+    return a
+
+unopt = timeit('bubblesort(array_to_sort)', 'from __main__ import bubblesort, array_to_sort', number=10000)
+opt = timeit('bubblesort_optimized(array_to_sort)', 'from __main__ import bubblesort_optimized, array_to_sort', number=10000)
+
+print(f'Разница между временем выполнения обычной функции и оптимизированной составляет: {unopt-opt} секунд\n' 
+      f'Что показывает, что оптимизированная функция быстрее в {unopt/opt} раз')
+
+'''
+Оптимизация обычным break'ом дала прирост эффективности в 11 раз, фантастика. Лишний перебор значений 
+списка все еще дорого стоит.
+'''
